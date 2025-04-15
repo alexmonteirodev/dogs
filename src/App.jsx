b3ea0b9304a1b56cdfa53c7f5c82e32825d9cfe9
alexmonteirodev <159573548+alexmonteirodev@users.noreply.github.com>
2025-04-15 16:21:25 +0200

    ambiente de desenvolvimento vite instalado

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const App = () => {
+  // const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div>teste</div>
+    </>
+  );
+};
+
+export default App;
