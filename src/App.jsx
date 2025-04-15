d93c51015d10520b318811432eed83edf07edb19
alexmonteirodev <159573548+alexmonteirodev@users.noreply.github.com>
2025-04-15 21:05:09 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
 import React from "react";
 
 const App = () => {
-  // const [count, setCount] = useState(0);
-
   return (
     <>
-      <div>App React</div>
+      <div></div>
+      <div></div>
     </>
   );
 };
