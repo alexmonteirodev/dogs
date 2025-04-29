d534b39d64e768674cd08962fc79fab6c1fe84d6
alexmonteirodev <159573548+alexmonteirodev@users.noreply.github.com>
2025-04-30 00:07:20 +0200

    ambiente vite preparado

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,7 @@
 import React from "react";
 
 const App = () => {
-  const [contar, setContar] = React.useState(1);
-  const [items, setItems] = React.useState(["Item 1"]);
-
-  function handleClick() {
-    setContar((contar) => {
-      return contar + 1;
-    });
-    setItems((items) => [...items, "Item " + (contar + 1)]);
-  }
-
-  return (
-    <>
-      {items.map((item) => (
-        <li key={item}>{item}</li>
-      ))}
-      <button onClick={handleClick}>{contar}</button>
-    </>
-  );
+  return <div>teste sdfdfd</div>;
 };
 
 export default App;
