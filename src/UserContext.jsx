55b334b0452a48c48120336efaf5a2d8eb1cc2a6
alexmonteirodev <159573548+alexmonteirodev@users.noreply.github.com>
2025-05-07 16:00:01 +0200

    update deploy

--- src/UserContext.jsx
+++ src/UserContext.jsx
@@ -1,6 +1,7 @@
 // esse componente serve para dar acesso global do usuário na Aplicação
 import React from "react";
 import { TOKEN_POST, TOKEN_VALIDATE_POST, USER_GET } from "./api";
+import { useNavigate } from "react-router-dom";
 
 export const UserContext = React.createContext();
 
@@ -9,6 +10,7 @@ export const UserStorage = ({ children }) => {
   const [login, setLogin] = React.useState(null);
   const [loading, setLoading] = React.useState(false);
   const [error, setError] = React.useState(null);
+  const navigate = useNavigate();
 
   React.useEffect(() => {
     function autoLogin() {
