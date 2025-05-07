7a96d73c07016262128143ba64adc016c99c6ea0
alexmonteirodev <159573548+alexmonteirodev@users.noreply.github.com>
2025-05-07 11:44:31 +0200

    footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,8 +1,14 @@
 import React from "react";
 import styles from "./Footer.module.css";
+import Logo from "../Assets/dogs-footer.svg?react";
 
 const Footer = () => {
-  return <div></div>;
+  return (
+    <footer className={styles.footer}>
+      <Logo />
+      <p>Dogs. Alguns direitos reservados.</p>
+    </footer>
+  );
 };
 
 export default Footer;
